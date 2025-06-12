ae74db53f9670beb2725ee69cf2a3e9dbff71a62
alriansyah <alriansyah1218@gmail.com>
2025-06-12 21:57:51 +0700

    feat: learn ts dasar setup projek typescript

---
+++ 1-ts-dasar/tests/hello.test.ts
@@ -0,0 +1,8 @@
+describe("Hello", function () {
+  it("should say hello", function () {
+    const name = "Hello Eko";
+    expect(name).toBe("Hello Eko");
+  });
+});
+
+// untuk running "npx jest"
