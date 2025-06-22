dc108d4c67cde8a0aa09b23114713612bcdd7226
alriansyah <alriansyah1218@gmail.com>
2025-06-22 20:36:33 +0700

    feat: learn ts dasar object type

---
+++ 1-ts-dasar/tests/9-object.test.ts
@@ -0,0 +1,22 @@
+/*
+    Object Type :
+    - Pada kasus yang sederhana, kadang membuat alias terlalu bertele-tele, kita juga bisa membuat tipe data JavaScript Object secara langsung dengan mendeklarasikan detail type nya ketika membuat  variabel nya
+    - Hal ini lebih sederhana dibandingkan membuat Type terlebih dahulu
+    - tidak disarankan menggunakannya pada kasus yang kompleks
+*/
+
+describe("Object", function () {
+  it("should support in typescript", function () {
+    const person: { id: string; name: string; hobbies?: string[] } = {
+      id: "1",
+      name: "Eko",
+    };
+
+    console.info(person);
+
+    person.id = "2";
+    person.name = "Kurniawan";
+
+    console.info(person);
+  });
+});
