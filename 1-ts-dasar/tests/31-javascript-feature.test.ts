f5fd554560e5a5f0eb55260d5f6bbe8c19122fef
alriansyah <alriansyah1218@gmail.com>
2025-07-02 22:08:56 +0700

    feat: learn ts dasar javascript feature

---
+++ 1-ts-dasar/tests/31-javascript-feature.test.ts
@@ -0,0 +1,9 @@
+/*
+    JavaScript Feature :
+    - Semua fitur yang pernah kita bahas di kelas JavaScript, bisa dilakukan di TypeScript
+    - Seperti Operator Matematika, Perbandingan, Logika, String Template, Optional Chaining, With Statement, Default Parameter, Function Generator, Getter Setter, Destructuring, Modules, Standard Library dan lain-lain
+    - Yang membedakan adalah, pada fitur TypeScript, karena Strongly Type, kita harus menentukan tipe data dari tiap variabel dan parameter
+    - Pada kasus jika kita ingin seperti di JavaScript, dimana variabel atau parameter bisa menggunakan tipe data apapun, maka kita harus menggunakan tipe data Any di TypeScript
+    - Ketika menggunakan typescript, diusahaakan sebisa mungkin hindari penggunaan tipe data any
+    - Jika memang ada data yang spesifik, sebaiknya kita bikin interface atau type alias untuk tipe data tersebut
+*/
