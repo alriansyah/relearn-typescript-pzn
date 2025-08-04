29ef211d582e6b9608874b09655cdb4c7ab1b2f0
alriansyah <alriansyah1218@gmail.com>
2025-08-04 20:36:46 +0700

    feat: learn ts generic intro

---
+++ 3-ts-generic/tests/1-intro.test.ts
@@ -0,0 +1,6 @@
+/*
+    TypeScript Generic :
+    - Generic adalah fitur dimana kita bisa membuat kode yang sama, dan bisa digunakan berulang kali dengan menggunakan tipe data yang berbeda
+    - Sebelumnya, saat kita ingin menggunakan tipe data yang bisa berbeda untuk variabel atau parameter, kita menggunakan tipe data any
+    - Dengan menggunakan Generic, kita bisa melakukan perubahan tipe data ketika digunakan, sehingga lebih aman karena tidak perlu menggunakan tipe data seperti any
+*/
