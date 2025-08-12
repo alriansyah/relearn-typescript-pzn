099817dd4df74166674d69bec67b071695296f38
alriansyah <alriansyah1218@gmail.com>
2025-08-12 21:21:39 +0700

    feat: learn ts validation custom validation message

---
+++ 4-ts-validation/tests/8-custom-validation-message.test.ts
@@ -0,0 +1,31 @@
+/*
+    Custom Validation Message :
+    - Saat kita menggunakan Zod, secara default sudah terdapat message error
+    - Namun, jika kita ingin ubah message errornya, kita bisa ubah ketika menggunakan method di schema
+*/
+
+import { z } from "zod";
+
+describe("object validation", () => {
+  it("should can validate object with message", async () => {
+    const loginSchema = z.object({
+      username: z.string().email("username harus email"),
+      password: z
+        .string()
+        .min(6, "password min harus 6 karakter")
+        .max(20, "password max harus 20 karakter"),
+    });
+
+    const request = {
+      username: "eko",
+      password: "12",
+    };
+
+    try {
+      const result = loginSchema.parse(request);
+      console.info(result);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+});
